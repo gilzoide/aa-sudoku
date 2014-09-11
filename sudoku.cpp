42166f2477aa1500fd066603fcbd1a07bd6e5a4f
Leonardo Sampaio <leosfr1419@gmail.com>
2014-09-10 21:14:29 -0300

    NOVO: documentação da classe Sudoku AWESOME FACTOR: awesome docs

--- sudoku.cpp
+++ sudoku.cpp
@@ -5,16 +5,9 @@ Sudoku::Sudoku () : cont (0) {}
 Sudoku::Sudoku (bool pega_da_stdin) {
 	Sudoku ();
 	if (pega_da_stdin) {
-		for (int i = 0; i < tam_sudoku; i++) {
-			for (int j = 0; j < tam_sudoku; j++) {
-				cin >> matriz[i][j];
-			}
-		}
+        cin >> this;
 	}
 }
-
-
-
 
 ostream& operator<< (ostream& os, const Sudoku& sud) {
 	for (int i = 0; i < Sudoku::tam_sudoku; i++) {
@@ -25,4 +18,12 @@ ostream& operator<< (ostream& os, const Sudoku& sud) {
 	}
 
 	return os;
+}
+
+istream& operator>> (istream& is, Sudoku& sud) {
+    for (int i = 0; i < tam_sudoku; i++) {
+        for (int j = 0; j < tam_sudoku; j++) {
+            is >> sud.matriz[i][j];
+        }
+    }
 }
--- sudoku.hpp
+++ sudoku.hpp
@@ -3,25 +3,99 @@
 
 #include <iostream>
 
-using namespace std;
-
 class Sudoku {
-	friend ostream& operator<< (ostream& os, const Sudoku& sud);
+    
+	friend std::ostream
+    & operator<< (std::ostream& os, const Sudoku& sud);
+    friend std::istream& operator>> (std::istream& is, const Sudoku& sud);
+
 public:
 	const static int altura_bloco = 3;
 	const static int largura_bloco = 3;
 	const static int tam_sudoku = altura_bloco * largura_bloco;
 
-	// Ctor
+    /**
+     * Construtor Default
+     * sem argumentos
+     */
 	Sudoku ();
-	// Ctor que já pega valores de 'input'
+    
+	/**
+     * Construtor que lê matriz de entradas
+     * do usuário e constrói o jogo de Sudoku
+     *
+     * @param pega_da_stdin
+     * Flag paraindicar se a leitura de input deve
+     * ser realizada ou não
+     *
+     */
 	Sudoku (bool pega_da_stdin);
-
-
+    
+    /**
+     * Resolve um jogo de Sudoku utilizando 
+     * um algorítmo de backtracking com diversas
+     * modificações voltadas a um melhor desempenho
+     *
+     * @param verificacao_adiante
+     * flag: backtracking com verificação adiante. backtracking
+     * é realizado quando uma variável fica sem nenhum valor
+     * disponível;
+     *
+     * @param verificacao_adiante_e_MVR
+     * flag: backtracking com verificação adiante e mínimos 
+     * valores remanescentes. Idem ao anterior com a adição da
+     * heurística MVR para decidir a próxima variável.
+     */
+    void solve(bool verificacao_adiante = false, bool verificacao_adiante_e_MVR = false);
+    
 
 private:
-	int matriz[tam_sudoku][tam_sudoku];
+	
+    /// matriz original do jogo
+    int matriz[tam_sudoku][tam_sudoku];
+    
+    /// matriz com possível solução
+    int solucao[tam_sudoku][tam_sudoku];
+    
+    /// contador de atribuições
 	int cont;
+    
+    /**
+     * Executa um algorítmo recursivo do tipo 
+     * backtracking para resolver o jogo de Sudoku
+     *
+     * @return
+     * retorna verdadeiro para solução encontrada
+     * falsa no caso contrário
+     */
+    bool backtracking_recursivo();
+    
+    /**
+     * Encontra a próxima casa
+     * do jogo que deve ser trabalhada
+     *
+     * @return 
+     * posição da próxima casa em um pair<x, y>
+     */
+    std::pair<int, int> proxima_casa();
+    
+    /**
+     * Verifica se o jogo é uma solução válida
+     *
+     * @return 
+     * verdadeiro para jogo válido, falso caso contrário
+     */
+    bool eh_um_jogo_valido();
+    
+    /**
+     * Verifica se o jogo é uma solução
+     * (não possui nenhuma casa com 0's)
+     * Não checa a validade do jogo
+     *
+     * @return
+     * verdadeiro para solução, falso caso contrário
+     */
+    bool eh_uma_solucao();
 };
 
 #endif
